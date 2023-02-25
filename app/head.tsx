9e19e143c15808577f1ba1a4a97ea735b9a4d6a9
Tufan Barış Yıldırım <tufanbarisyildirim@gmail.com>
2023-02-25 11:43:08 +0300

    update metadata

--- app/head.tsx
+++ app/head.tsx
@@ -6,6 +6,9 @@ export default function Head() {
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#000000" />
 
+      <meta name="keywords" content="rpc.ag, blockchain infrastructure, node providers, developer community, endpoint, project owners, secure node sharing">
+      <meta name="author" content="rpc.ag">
+
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
       <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;500;700&display=swap" rel="stylesheet"></link>
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { Header } from "@components";
 import { Footer } from "@components";
 
 export const metadata = {
-  title: "RPC",
-  description: "RPC",
+  title: "RPC.ag - Blockchain Infrastructure for Developers",
+  description: "RPC.ag is a comprehensive blockchain infrastructure platform designed to support developers and project owners in the blockchain space. Join our community today and start building the next generation of blockchain applications with RPC.ag.",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
